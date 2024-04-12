50b45323a70268150a3558acf9461fc75171240c
Zachary Harris <the1zachharris@gmail.com>
2024-04-12 13:19:48 -0500

    server is up and working localy with get all api call

---
+++ CodeSnipsAPI/Migrations/20240412181011_UserInfoDBInitalMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeSnipsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInfoDBInitalMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Snippets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Language = table.Column<string>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Snippets", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Snippets",
+                columns: new[] { "Id", "Code", "Language" },
+                values: new object[] { 1, "print('Hello, World!')", "Python" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Snippets");
+        }
+    }
+}
