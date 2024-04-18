11a547598c01a6f1130d10273861f882fd25bf89
Zachary Harris <the1zachharris@gmail.com>
2024-04-18 15:55:43 -0500

    setting up users

---
+++ CodeSnipsAPI/Models/UserForCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeSnipsAPI.Models
+{
+    public class UserForCreationDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [PasswordPropertyText]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
