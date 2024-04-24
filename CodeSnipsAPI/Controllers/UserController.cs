3c0d0e5b88938d5dc2de8735d36192c61aba665e
Zachary Harris <the1zachharris@gmail.com>
2024-04-24 11:20:06 -0500

    started working on users

--- CodeSnipsAPI/Controllers/UserController.cs
+++ CodeSnipsAPI/Controllers/UserController.cs
@@ -1,6 +1,24 @@
-﻿namespace CodeSnipsAPI.Controllers
+﻿
+using AutoMapper;
+using CodeSnipsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeSnipsAPI.Controllers
 {
-    public class UserController
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
     {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<UserDto>> GetUser(int id)
+        //{
+
+        //}
     }
 }
